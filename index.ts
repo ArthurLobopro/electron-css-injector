f6b132b7235f0e9fd279ef84cb073880aa637409
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-03-09 19:46:41 -0300

    feat: add id identifier

--- index.ts
+++ index.ts
@@ -3,11 +3,16 @@ import path from "path"
 /**
  * Injects a CSS file into the DOM
  * @param css_path The path to the CSS file
+ * @param css_id The ID to identify the CSS file already injected
  */
-export function injectCSS(css_path: string) {
+export function injectCSS(css_path: string, css_id?: string) {
     const links = Array.from(document.querySelectorAll('link'))
 
     const css_exists = links.find(link => {
+        if (css_id && link.dataset.id === css_id) {
+            return true
+        }
+
         return (
             path.normalize(link.getAttribute('href') as string).replace(/file:\\\\/g, "") ===
             path.normalize(css_path)
@@ -18,6 +23,9 @@ export function injectCSS(css_path: string) {
         const css = document.createElement('link')
         css.rel = "stylesheet"
         css.href = css_path
+        if (css_id) {
+            css.dataset.id = css_id
+        }
         document.head.appendChild(css)
     }
 }
