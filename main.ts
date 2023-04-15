9507769c79b91eb75be012ad9991247c1d760c15
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-04-15 15:41:32 -0300

    fix: exports declarations and  protocol verification

--- main.ts
+++ main.ts
@@ -3,10 +3,15 @@ import { BrowserWindow, ipcMain } from "electron"
 ipcMain.on("create-css-protocol", (event) => {
     const win = BrowserWindow.fromId(event.sender.id) as BrowserWindow
 
-    win.webContents.session.protocol.registerFileProtocol("css", (request, callback) => {
-        const url = request.url.substring(6)
-        console.log(url)
-        callback({ path: url })
-    })
-})
+    const alreadyRegistered = win.webContents.session.protocol.isProtocolRegistered("css")
 
+    if (alreadyRegistered) {
+        console.warn("CSS protocol already registered")
+    } else {
+        win.webContents.session.protocol.registerFileProtocol("css", (request, callback) => {
+            const url = request.url.substring(6)
+            console.log(url)
+            callback({ path: url })
+        })
+    }
+})
