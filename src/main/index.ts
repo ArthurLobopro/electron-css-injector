584b72d50434dfe047abd65bfdc5b61b6a09d462
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-12-22 22:52:33 -0300

    chore: remove commented section

--- src/main/index.ts
+++ src/main/index.ts
@@ -15,21 +15,3 @@ if (app.isReady()) {
 } else {
     app.on("ready", createCSSProtocol)
 }
-
-// ipcMain.on("create-css-protocol", (event) => {
-//     const win = BrowserWindow.fromId(event.sender.id) as BrowserWindow
-
-//     const protocol_regex = /^css:(\/\/)/g
-
-//     const alreadyRegistered = win.webContents.session.protocol.isProtocolRegistered("css")
-
-//     if (alreadyRegistered) {
-//         console.log("CSS protocol already registered")
-//     } else {
-//         win.webContents.session.protocol.registerFileProtocol("css", (request, callback) => {
-//             const url = request.url.replace(protocol_regex, "")
-//             console.log(url)
-//             callback({ path: path.normalize(url) })
-//         })
-//     }
-// })
